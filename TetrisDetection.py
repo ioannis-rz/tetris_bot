961085daff37aac56c940351b975a9190a012452
Juan Arteaga <juanluis.arteaga.mantovani1@gmail.com>
2026-03-15 16:24:55 -0500

    refactor: reorganize Tetris game structure and remove unused functions

--- TetrisDetection.py
+++ TetrisDetection.py
@@ -4,6 +4,7 @@ import dxcam
 import debug
 from pynput.keyboard import Controller, Key
 import time
+
 TETROMINOS = {
 
     "I": [
@@ -90,7 +91,7 @@ MOVE_RIGHT = Key.right
 MOVE_LEFT = Key.left
 MOVE_DOWN = Key.down
 SNAP_DOWN = Key.space
-ROTATE = "z"
+ROTATELEFT = "z"
 SAVE = 'c'
 
 def count_complete_lines(board):
@@ -128,44 +129,6 @@ def get_column_heights(board):
 
     return heights
 
-def compute_sample_points(x, y, w, h):
-
-    points = []
-
-    cell_w = w / 10
-    cell_h = h / 20
-
-    for row in range(20):
-
-        row_points = []
-
-        for col in range(10):
-
-            px = int(x + col*cell_w + cell_w/2)
-            py = int(y + row*cell_h + cell_h/2)
-
-            row_points.append((px,py))
-
-        points.append(row_points)
-    print(points)
-    return points
-
-def update_board_state(frame, sample_points, board):
-    for row in range(20):
-        for col in range(10):
-
-            px, py = sample_points[row][col]
-
-            region = frame[py-2:py+3, px-2:px+3]
-
-            b, g, r = region.mean(axis=(0,1))
-            brightness = (b + g + r) / 3
-
-            if brightness < 40 and b < 50 and g < 50 and r < 50:
-                board[row, col] = 0 
-            else:
-                board[row, col] = 1
-
 def count_holes(board):
 
     holes = 0
@@ -183,65 +146,6 @@ def count_holes(board):
                 holes += 1
 
     return holes
-
-def correct_board_state(board):
-    logic_board = board.copy()
-    logic_board[0:4, :] = 0
-    return logic_board
-
-def get_falling_piece(board):
-
-    visited = set()
-
-    for r in range(4):
-        for c in range(10):
-
-            if board[r][c] == 1 and (r,c) not in visited:
-
-                stack = [(r,c)]
-                piece = []
-
-                while stack:
-
-                    cr, cc = stack.pop()
-
-                    if (cr,cc) in visited:
-                        continue
-                    if cr < 0 or cr >= 20 or cc < 0 or cc >= 10:
-                        continue
-                    if board[cr][cc] == 0:
-                        continue
-
-                    visited.add((cr,cc))
-                    piece.append((cr,cc))
-
-                    stack.extend([
-                        (cr+1,cc),(cr-1,cc),
-                        (cr,cc+1),(cr,cc-1)
-                    ])
-
-                if len(piece) == 4:
-
-                    # normalize coordinates
-                    min_r = min(r for r,c in piece)
-                    min_c = min(c for r,c in piece)
-
-                    norm = [(r-min_r, c-min_c) for r,c in piece]
-
-                    h = max(r for r,c in norm) + 1
-                    w = max(c for r,c in norm) + 1
-
-                    mat = [[0]*w for _ in range(h)]
-
-                    for r,c in norm:
-                        mat[r][c] = 1
-
-                    # compare with tetromino dictionary
-                    for name, rotations in TETROMINOS.items():
-                        for i, rot in enumerate(rotations):
-                            if mat == rot:
-                                return name, i, min_c
-    return None
 
 def evaluate_board(board):
 
@@ -288,66 +192,202 @@ def place_piece(board, piece, row, col):
 
     return new
 
-def find_best_move(board, piece_name):
+class Enviroment:
+    def __init__(self):
+        self.camera = dxcam.create(output_color="BGR")
+        self.camera.start(target_fps=60, video_mode=True)
+        self.keyboard = Controller()
 
-    best_score = -1e9
-    best_move = None
+    def get_frame(self):
+        return self.camera.get_latest_frame()
 
-    for i, rotation in enumerate(TETROMINOS[piece_name]):
+    def act(self, moves):
+        for move in moves:
+            self.keyboard.tap(move)
+            time.sleep(0.2)    
 
-        h = len(rotation)
-        w = len(rotation[0])
+class AgentPerception:
 
-        for col in range(10 - w + 1):
+    def __init__(self, x, y, w, h):
+        self.sample_points = self.compute_sample_points(x,y,w,h)   
+        self.board = np.zeros((20,10))
 
-            row = 0
+    def get_falling_piece(self):
+        visited = set()
 
-            # drop piece
-            while not collision(board, rotation, row+1, col):
-                row += 1
+        for r in range(4):
+            for c in range(10):
 
-            new_board = place_piece(board, rotation, row, col)
+                if self.board[r][c] == 1 and (r,c) not in visited:
 
-            score = evaluate_board(new_board)
+                    stack = [(r,c)]
+                    piece = []
 
-            if score > best_score:
-                best_score = score
-                best_move = (i, col)
+                    while stack:
 
-    return best_move
+                        cr, cc = stack.pop()
 
-def calculate_moves(piece, best_move):
+                        if (cr,cc) in visited:
+                            continue
+                        if cr < 0 or cr >= 20 or cc < 0 or cc >= 10:
+                            continue
+                        if self.board[cr][cc] == 0:
+                            continue
 
-    piece_name, current_rot, current_col = piece
-    target_rot, target_col = best_move
+                        visited.add((cr,cc))
+                        piece.append((cr,cc))
 
-    moves = []
+                        stack.extend([
+                            (cr+1,cc),(cr-1,cc),
+                            (cr,cc+1),(cr,cc-1)
+                        ])
 
-    # calcular rotaciones necesarias
-    rotations_needed = (target_rot - current_rot) % len(TETROMINOS[piece_name])
+                    if len(piece) == 4:
 
-    for _ in range(rotations_needed):
-        moves.append(ROTATE)
+                        # normalize coordinates
+                        min_r = min(r for r,c in piece)
+                        min_c = min(c for r,c in piece)
 
-    # calcular movimiento horizontal
-    diff = target_col - current_col
+                        norm = [(r-min_r, c-min_c) for r,c in piece]
 
-    if diff > 0:
-        for _ in range(diff):
-            moves.append(MOVE_RIGHT)
+                        h = max(r for r,c in norm) + 1
+                        w = max(c for r,c in norm) + 1
 
-    elif diff < 0:
-        for _ in range(-diff):
-            moves.append(MOVE_LEFT)
+                        mat = [[0]*w for _ in range(h)]
 
-    # hard drop al final
-    moves.append(SNAP_DOWN)
+                        for r,c in norm:
+                            mat[r][c] = 1
 
-    return moves
+                        # compare with tetromino dictionary
+                        for name, rotations in TETROMINOS.items():
+                            for i, rot in enumerate(rotations):
+                                if mat == rot:
+                                    return name, i, min_c
+        return None
+
+    def update_board_state(self, frame):
+        for row in range(20):
+            for col in range(10):
+
+                px, py = self.sample_points[row][col]
+
+                region = frame[py-2:py+3, px-2:px+3]
+
+                b, g, r = region.mean(axis=(0,1))
+                brightness = (b + g + r) / 3
+
+                if brightness < 40 and b < 50 and g < 50 and r < 50:
+                    self.board[row, col] = 0 
+                else:
+                    self.board[row, col] = 1
+
+    def compute_sample_points(self, x, y, w, h):
+
+        points = []
+
+        cell_w = w / 10
+        cell_h = h / 20
+
+        for row in range(20):
+
+            row_points = []
+
+            for col in range(10):
+
+                px = int(x + col*cell_w + cell_w/2)
+                py = int(y + row*cell_h + cell_h/2)
+
+                row_points.append((px,py))
+
+            points.append(row_points)
+        print(points)
+        return points
+
+class Agent:
+    
+    def __init__(self):
+        self.logic_board = np.zeros((20,10))
+        self.piece_active = False
+
+    def update(self, board, piece):
+       
+        if piece is not None and not self.piece_active:
+
+            self.piece_active = True
+            self.correct_board_state(board)
+            best_move = self.find_best_move(self.logic_board, piece[0])
+            moves = self.calculate_moves(piece, best_move)
+            return moves
+
+        if piece is None:
+            self.piece_active = False
+
+        return None 
+
+    def correct_board_state(self, board):
+        self.logic_board = board.copy()
+        self.logic_board[0:4, :] = 0
+        return self.logic_board
+
+    def find_best_move(self, board, piece_name):
+
+        best_score = -1e9
+        best_move = None
+
+        for i, rotation in enumerate(TETROMINOS[piece_name]):
+
+            h = len(rotation)
+            w = len(rotation[0])
+
+            for col in range(10 - w + 1):
+
+                row = 0
+
+                # drop piece
+                while not collision(board, rotation, row+1, col):
+                    row += 1
+
+                new_board = place_piece(board, rotation, row, col)
+
+                score = evaluate_board(new_board)
+
+                if score > best_score:
+                    best_score = score
+                    best_move = (i, col)
+
+        return best_move
+
+    def calculate_moves(self, piece, best_move):
+
+        piece_name, current_rot, current_col = piece
+        target_rot, target_col = best_move
+
+        moves = []
+
+        # calcular rotaciones necesarias
+        rotations_needed = (current_rot - target_rot) % len(TETROMINOS[piece_name])
+
+        for _ in range(rotations_needed):
+            moves.append(ROTATELEFT)
+
+        # calcular movimiento horizontal
+        diff = target_col - current_col
+
+        if diff > 0:
+            for _ in range(diff):
+                moves.append(MOVE_RIGHT)
+
+        elif diff < 0:
+            for _ in range(-diff):
+                moves.append(MOVE_LEFT)
+
+        # hard drop al final
+        moves.append(SNAP_DOWN)
+
+        return moves
+
 
 # cosas que se ejecutan una sola vez
-board = np.zeros((20,10))
-logic_board = np.zeros((20,10))
 #print(board)
 # en base a la prueba, se obtuvo estas dimensiones en fullscreen
 x = 808
@@ -359,18 +399,19 @@ x = 420
 y = 404
 w = 159
 h = 315
-camera = dxcam.create(output_color="BGR")
-camera.start(target_fps=60, video_mode=True)
-samplePoints = compute_sample_points(x,y,w,h)
-keyboard = Controller()
+# samplePoints = compute_sample_points(x,y,w,h)
 
 #preview = debug.draw_sample_points(frame.copy(), samplePoints) # para debug
 # crear el objeto para capturar la imagen
 
 print("Press S to start the bot")
 
+env = Enviroment()
+vision = AgentPerception(x,y,w,h)
+agent = Agent()
+
 while True:
-    frame = camera.get_latest_frame()
+    frame = env.get_frame()
     frame = cv2.resize(frame, None, fx = 0.25, fy = 0.25, interpolation=cv2.INTER_AREA)
     cv2.imshow("preview", frame)
     key = cv2.waitKey(1) & 0xFF
@@ -380,40 +421,34 @@ while True:
         cv2.destroyAllWindows()
         break
 
-piece_active = False
-piece = None
 #print(TETROMINOS["I"])
+last_board = None
+
 while True:
 
-    frame = camera.get_latest_frame()
+    frame = env.get_frame()
 
-    update_board_state(frame, samplePoints, board) # calcular el estado del tablero de juego
+    vision.update_board_state(frame) # calcular el estado del tablero de juego
+    # print(vision.board)
+    if last_board is None or not np.array_equal(vision.board, last_board):
+        print(vision.board)
+        print()
+        last_board = vision.board.copy()
+
+    piece = vision.get_falling_piece() # capturtar la pieza nueva que cae, si easta incompleta, retorna None
+    # print(piece)
+
+    # moves = agent.update(vision.board.copy(), piece)
+
+    #if moves:
+    #   env.act(moves)
     
-    piece = get_falling_piece(board) # capturtar la pieza nueva que cae, si easta incompleta, retorna None
-
-    if piece is not None and not piece_active: # si la pieza se detecta y no hay pieza previamente detectada
-        piece_active = True
-        print("New piece detected")
-        print(piece)
-        print(piece[0])
-        logic_board = correct_board_state(board.copy())
-        best_move = find_best_move(logic_board, piece[0])
-
-        moves = calculate_moves(piece, best_move)
-
-        for move in moves:
-            keyboard.tap(move)
-            time.sleep(0.2)
-
-    if piece is None:
-        piece_active = False
-
-    preview = debug.draw_board_state(frame, samplePoints, board)
+    preview = debug.draw_board_state(frame, vision.sample_points, vision.board)
     preview = cv2.resize(preview, None, fx = 0.5, fy = 0.5, interpolation=cv2.INTER_AREA)
     cv2.imshow("debug screen", preview)
     #print(board)
     if cv2.waitKey(1) == 27: # esc key
-        camera.stop()
+        env.camera.stop()
         cv2.destroyAllWindows()
         break
 
